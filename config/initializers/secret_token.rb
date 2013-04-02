d39ec01bff5fa6793f32a6e6f560db5f89baf917
Tanner Mares <tanner@ministrycentered.com>
2013-04-02 09:24:47 -0700

    created rails app, initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FullStackTalks::Application.config.secret_token = 'ba0100c46771f40c51761db4d011addf5cb19bf620043f286e1df4c5dcd891b552a06eceaedf6b4c2ea0700ed1daf35795948c7dd6b02748c140b00029499909'
