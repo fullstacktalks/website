6c8ab287a4ff43b9dcf2b50952ef1c729e76dcbd
Michael Chan <mijoch@gmail.com>
2013-04-08 15:03:37 -0700

    update event model relationship to talks

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ActiveRecord::Base
   attr_accessible :date, :description, :location, :title
 
   has_and_belongs_to_many :people
+  has_many :talks
 
   validates_presence_of :title, :date
 end
