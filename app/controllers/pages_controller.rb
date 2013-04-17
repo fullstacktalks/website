9c7ddcbf8d98d5e94b03644a489d6639ca065abb
Michael Chan <mijoch@gmail.com>
2013-04-17 09:00:20 -0700

    move keys and tokens out of git

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,16 @@
 class PagesController < ApplicationController
   def index
-    @people = Person.all
-    @next_event = Event.includes(:talks).where("date >= ?", Time.now).order("date").first
-    @next_event_talks = @next_event.talks if @next_event.present?
-    @next_event_talks ||= []
-  end
+    RMeetup::Client.api_key = ENV['MEETUP_API_KEY']
+    results = RMeetup::Client.fetch(:events,{:zip => "ZIP_CODE"})
+    results.each do |result|
+      # Do something with the result
+    end
+end
+
   def about
     @company = "Full Stack Talks"
   end
+
   def contact
   end
 end
