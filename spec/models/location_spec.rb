edee2054e90aff0d2f4e04da2127672a5c5dcae9
Tanner Mares <tannermares@gmail.com>
2013-04-18 10:40:19 -0700

    added spec for location class

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Location do
-
+  describe '.new_from_meetup' do
+    it 'creates an object using attributes from a request' do
+      request = OpenStruct.new(venue_name: 'Awesome Building', venue_address1: '1234 five way')
+      event = Event.new_from_meetup(request)
+      event.location.name.should == 'Awesome Building'
+      event.location.address.should == '1234 five way'
+    end
+  end
 end
