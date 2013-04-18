08eb2970c946197892feed67735b56abefc7d718
Tanner Mares <tannermares@gmail.com>
2013-04-17 17:16:06 -0700

    added spec for new_from_meetup method

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,4 +6,13 @@ describe Event do
 
   it { should have_many(:talks) }
   it { should have_and_belong_to_many(:people) }
+
+  describe '.new_from_meetup' do
+    it 'creates an object using attributes from a request' do
+      request = OpenStruct.new(description: 'Foo', event_url: 'http://example.com')
+      event = Event.new_from_meetup(request)
+      event.description.should == 'Foo'
+      event.url.should == 'http://example.com'
+    end
+  end
 end
