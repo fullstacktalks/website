3a6b0e31a789a05241a3be3a6bc4383b9d5d1133
Tanner Mares <tannermares@gmail.com>
2013-04-18 14:16:43 -0700

    only set event if results are returned from meetup api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def index
     @results = RMeetup::Client.fetch(:events,{:group_id => "7290272"})
     @mail_to_url = CGI.escape("mailto:talks@fullstacktalks.com?Subject=I'm%20Interested")
-    @event = Event.new_from_meetup(@results[0])
+    @event = Event.new_from_meetup(@results[0]) unless @results[0].blank?
   end
 
   def about
