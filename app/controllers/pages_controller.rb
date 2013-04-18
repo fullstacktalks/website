342748debb9d8a5f1852c30101a5916d94fe0e92
Tanner Mares <tannermares@gmail.com>
2013-04-17 17:12:53 -0700

    fixed indenting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     @results = RMeetup::Client.fetch(:events,{:group_id => "7290272", :zip => "92009"})
     @mail_to_url = CGI.escape("mailto:talks@fullstacktalks.com?Subject=I'm%20Interested")
     @event = Event.new_from_meetup(@results[0])
-end
+  end
 
   def about
     @company = "Full Stack Talks"
