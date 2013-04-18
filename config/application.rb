191a3ce16595cff33a5f27cae15114138d57df35
Tanner Mares <tannermares@gmail.com>
2013-04-17 17:07:46 -0700

    moved application secret code into application config

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,8 @@ module FullStackTalks
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    config.secret_token = ENV['APPLICATION_SECRET_TOKEN']
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
