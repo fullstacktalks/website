627ed026080b01ed66a9282043c36a958207a02d
Michael Chan <mijoch@gmail.com>
2013-04-22 10:14:46 -0700

    add Google Analytics to productin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,7 @@ FullStackTalks::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.gem 'rack-google-analytics', :lib => 'rack/google-analytics'
+  config.middleware.use Rack::GoogleAnalytics, :tracker => 'UA-40335994-1'
 end
