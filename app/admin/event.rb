e80f8d408371e2e9f1bd84486a505a38dc090943
Kevin Thompson <kevin@kevinthompson.info>
2014-09-18 21:46:50 -0700

    Add date picker to event form

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Event do
 
+  form :html => { :enctype => 'multipart/form-data' } do |f|
+    f.inputs 'Event Details' do
+      f.input :starts_at, as: :datepicker
+      f.input :description
+      f.input :url
+    end
+    f.actions
+  end
+
 end
